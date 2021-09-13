bd905bfd9748ac23874d0171d9e8bd3c723c572a
shubhampatte <shubhampatte@gmail.com>
2021-09-14 01:12:42 +0530

    compute result in ascending

--- arithSortComp.sh
+++ arithSortComp.sh
@@ -13,34 +13,51 @@ comp4=$(($c+$a/$b))
 
 declare -A result
 
-result["1"]=$comp1
-result["2"]=$comp2
-result["3"]=$comp3
-result["4"]=$comp4
+result["0"]=$comp1
+result["1"]=$comp2
+result["2"]=$comp3
+result["3"]=$comp4
+
+declare -a Array
 
 num=0
-
-declare -a resultArray
+max=4
 
 while [ $num -lt 4 ]
 do
-	resultArray[((num++))]=${result[$num]}
+	Array[((num++))]=${result[$num]}
 done
+echo ${Array[*]}
 
-for (( i=0; i<4; i++ ))
+for (( i=0; i<$max; i++ ))
 do
-        for (( j=i+1; j<4; j++ ))
+        for (( j=i+1; j<$max; j++ ))
         do
-                if [ "${resultArray[i]}" -lt "${resultArray[j]}" ]
+                if [ ${Array[i]} -lt ${Array[j]} ]
                 then
-                        temp=${resultArray[i]}
-                        resultArray[i]=${resultArray[j]}
-                        resultArray[j]=$temp;
+                        temp=${Array[i]}
+                        Array[i]=${Array[j]}
+                        Array[j]=$temp;
                 fi
         done
+	echo "descending order:" ${Array[*]}
 done
 
-echo ${resultArray[*]}
+for (( i=0; i<$max; i++ ))
+do
+        for (( j=i+1; j<$max; j++ ))
+        do
+                if [ ${Array[i]} -gt ${Array[j]} ]
+                then
+                        temp=${Array[i]}
+                        Array[i]=${Array[j]}
+                        Array[j]=$temp;
+                fi
+        done
+
+	echo " ascending order : " ${Array[*]}
+
+done
 
 
 
